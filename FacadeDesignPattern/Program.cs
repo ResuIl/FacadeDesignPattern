501971d1cb6c59509ba9a075e28dcd6e18a6fee4
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-11-25 08:20:49 +0400

    Add project files.

---
+++ FacadeDesignPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public Customer(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }  
+}
+
+class BlackListService
+{
+    public bool CheckEmployeeIsTheBlackList(Customer customer) => true;
+}
+
+class CreditCardManager
+{
+    public void CreateCreditCart(Customer customer)
+    {
+        
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Customer Customer = new Customer("Resul", "Black");
+
+        CreditCardManager creditCardManager = new CreditCardManager(); ;
+        BlackListService blackListService = new BlackListService();
+
+        if (blackListService.CheckEmployeeIsTheBlackList(Customer))
+        {
+            creditCardManager.CreateCreditCart(Customer);
+        }
+    }
+}
